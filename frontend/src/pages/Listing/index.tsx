f6a594c079ea3b2ed61ecf2709416633bd86a574
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-01-12 16:35:27 -0400

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,11 @@
+import Pagination from "components/Pagination";
+
 function Listing() {
     return (
-        <h1>Página de listagem</h1>
+        <>
+            <Pagination />
+            <h1>Página de listagem</h1>
+        </>
     );
 }
 
