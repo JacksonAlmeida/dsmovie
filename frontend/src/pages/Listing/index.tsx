faebd03b07128d83244316d49629372da8ff9ad4
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-01-12 23:32:40 -0400

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
     return (
         <>
-            <Pagination />
-            <h1>Página de listagem</h1>
+            <div className="container">
+                <Pagination />
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
