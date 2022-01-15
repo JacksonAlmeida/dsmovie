48946023e653fc32123b3275868b48058cbd7995
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-01-15 13:56:59 -0400

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,20 +1,46 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "ultis/requests";
 
 function Listing() {
 
-    //forma errada
+
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=10$page=0`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                setPageNumber(data.number);
+                console.log(response.data);
+            });
+    }, []);
+
+    /*forma errada
     axios.get(`${BASE_URL}/movies?size=12&page=0`)
         .then(response => {
             console.log(response.data);
         });
 
+        -----------------------------------
+
+axios.get(`${BASE_URL}/movies?size=12&page=1`)
+    .then(response => {
+        const data = response.data as MoviePage;
+        setPageNumber(data.number);
+        console.log(response.data);
+    });
+
+    */
+
     return (
         <>
+            <p>{pageNumber}</p>
+            <Pagination />
             <div className="container">
-                <Pagination />
                 <div className="row">
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
